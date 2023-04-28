d7462ad8a0add32ef771fccfa30ba6efa48020c8
William Redenbaugh <wmredenbaugh@gmail.com>
2023-04-28 11:39:20 -0700

    added semaphore code

---
+++ os_sem.cpp
@@ -0,0 +1,73 @@
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "os_sem.h"
+#include "os_error.h"
+
+int os_sem_init(os_sem_t *sem, int count){
+    if(sem == NULL){
+        return OS_RET_NULL_PTR;
+    }
+    sem->sem = xSemaphoreCreateCounting(count, 0);
+    return OS_RET_OK;
+}
+
+int os_sem_entry(os_sem_t *sem, uint32_t timeout_ms){
+    if(sem == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    if (xSemaphoreTake(sem->sem, timeout_ms/portTICK_PERIOD_MS) == pdTRUE){
+        return OS_RET_OK;
+    }
+    else{
+        return OS_RET_MAX_RENTRANT;
+    }
+
+}
+
+int os_sem_entry_wait_indefinite(os_sem_t *sem){
+    if(sem == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    if (xSemaphoreTake(sem->sem, portMAX_DELAY) == pdTRUE){
+        return OS_RET_OK;
+    }
+    else{
+        return OS_RET_MAX_RENTRANT;
+    }
+
+    return OS_RET_OK;
+}
+
+int os_sem_entry_try(os_sem_t *sem){
+    if(sem == NULL){
+        return OS_RET_NULL_PTR;
+    }
+    if (xSemaphoreTake(sem->sem, 0) == pdTRUE){
+        return OS_RET_OK;
+    }
+    else{
+        return OS_RET_MAX_RENTRANT;
+    }
+}
+
+int os_sem_count(os_sem_t *sem){
+    if(sem == NULL){
+        return OS_RET_NULL_PTR;
+    }
+    return uxSemaphoreGetCount(sem->sem);
+}
+
+int os_sem_give(os_sem_t *sem){
+    if(sem == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    if(xSemaphoreGive(sem->sem) == pdTRUE){
+        return OS_RET_OK;
+    }
+    else{
+        return OS_RET_MAX_RENTRANT;
+    }
+}
---
+++ os_sem.h
@@ -0,0 +1,40 @@
+#ifndef _SEM_H
+#define _SEM_H
+
+#include "FreeRTOS.h"
+#include "semphr.h"
+
+typedef struct os_sem_t{
+    SemaphoreHandle_t sem;
+}os_sem_t;
+
+/**
+ * @brief Initializes a semaphore module
+ * @param os_sem_t 
+*/
+int os_sem_init(os_sem_t *sem, int count);
+
+/**
+ * @brief Allows us to acquuire our semaphore
+ * @param os_sem_t 
+*/
+int os_sem_entry(os_sem_t *sem, uint32_t timeout_ms);
+
+/**
+ * @brief How many re-entrants into the lock
+ * @param os_sem_t 
+*/
+int os_sem_count(os_sem_t *sem);
+
+/**
+ * @brief Waits indefinitely until the semaphore has a rentrant lock available
+ * @param os_sem_t 
+*/
+int os_sem_entry_wait_indefinite(os_sem_t *sem);
+
+/**
+ *  @brief Gives up count from semaphore
+ * @param os_sem_t 
+*/
+int os_sem_give(os_sem_t *sem);
+#endif
