97eeb7fc17c9fcdd6fd1c3fb5630d8be64696da7
William Redenbaugh <wmredenbaugh@gmail.com>
2023-04-28 11:25:31 -0700

    os_thread csal completed!

---
+++ os_thread.cpp
@@ -0,0 +1,57 @@
+#include "os_thread.h"
+
+os_thread_id_t os_add_thread(thread_func_t p, void * arg, int stack_size, void *stack){
+    os_thread_id_t id; 
+
+    if(stack == NULL){
+        xTaskCreate(p, 
+        "", 
+        stack_size, 
+        arg, 
+        0, 
+        &id.handle
+        );
+    }
+    else{
+        xTaskCreateStatic(
+            p, 
+            "",
+            stack_size, 
+            arg, 
+            0, 
+            (StackType_t*)stack, 
+            &id.buffer 
+        );
+    }
+
+    return id;
+}
+
+os_thread_id_t os_suspend_thread(os_thread_id_t target_thread_id){
+    vTaskSuspend(target_thread_id.handle);
+
+    return target_thread_id;
+}
+
+os_thread_id_t os_resume_thread(os_thread_id_t target_thread_id){
+    vTaskResume(target_thread_id.handle);
+
+    return target_thread_id;
+}
+
+os_thread_id_t os_kill_thread(os_thread_id_t target_thread_id){
+    vTaskDelete(target_thread_id.handle);
+
+    return target_thread_id;
+}
+
+os_thread_id_t os_current_id(void){
+    os_thread_id_t id; 
+    id.handle = xTaskGetCurrentTaskHandle();   
+
+    return id;
+}
+
+void os_kill_self_thread(void){
+    vTaskDelete(NULL);
+}
