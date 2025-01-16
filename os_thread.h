e449d1d42339f236c89c0b1542c008f255f2a61b
William Redenbaugh <wmredenbaugh@gmail.com>
2025-01-16 00:36:24 -0800

    separation packages

--- os_thread.h
+++ os_thread.h
@@ -1,8 +1,16 @@
 #ifndef _OS_THREAD_H
 #define _OS_THREAD_H
 
+#include "project_defs.h"
+
+#ifdef PLATFORM_ESP32
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#else
 #include "FreeRTOS.h"
 #include "task.h"
+#endif
+
 
 #include "csal_struct_definitions.h"
 #include "stdint.h"
